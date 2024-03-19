d1f9675ec1b997c63ea7cd58afa332af7fe72136
dutabrahmana <dutalinggau25@gmail.com>
2024-03-19 11:45:34 +0700

    membuat awalan

--- PraktikumKe3.4/PraktikumKe3.4.cpp
+++ PraktikumKe3.4/PraktikumKe3.4.cpp
@@ -1,23 +1,3 @@
 #include <iostream>
 using namespace std;
 
-int sisi;
-void inputdata()
-{
-	cout << "Masukan Nilai sisi :";
-	cin >> sisi;
-
-}
-int hitungluas()
-{
-	return sisi * sisi;
-}
-void display()
-{
-	cout << "Luas Persegi:" << hitungluas() << endl;
-}
-int main()
-{
-	inputdata();
-	display();
-}
