911e4349c558c896c6710d01967031a842638104
dutabrahmana <dutalinggau25@gmail.com>
2024-03-19 10:24:02 +0700

    membuat input variabel

--- PraktikumKe3_PemogramanDasar/PraktikumKe3_PemogramanDasar.cpp
+++ PraktikumKe3_PemogramanDasar/PraktikumKe3_PemogramanDasar.cpp
@@ -5,8 +5,16 @@ int main() {
 
 	int i;
 	int arr[5];
+	for (i = 60; i > 10; i -= 10) {
+		cout << i << " Selamat Pagi Dunia" << endl;
 
+	}
+	cout << " Nilai i Terakhir : " << i << endl;
 
 	
+
 	}
+	
+	}
+	
 }
