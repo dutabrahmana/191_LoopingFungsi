7e750e768b12add97ad2417f8697e0e584dde315
dutabrahmana <dutalinggau25@gmail.com>
2024-03-19 11:23:07 +0700

    membuat input void

---
+++ PraktikumKe3.3/PraktikumKe3.3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int panjang, lebar;
+void inputdata() {
+	cout << "Masukan Nilai Panjang : ";
+	cin >> panjang;
+	cout << "Masukan Nilai Lebar : ";
+	cin >> lebar;
+}
+
